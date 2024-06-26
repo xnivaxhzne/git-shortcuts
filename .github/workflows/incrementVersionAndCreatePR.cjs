fe701898bae7cce5ccfafb2ba81a3a2bf2aabf5b
xnivaxhzne <kavinkumar1543884@gmail.com>
2024-06-26 13:05:42 +0530

    Error

--- .github/workflows/incrementVersionAndCreatePR.cjs
+++ .github/workflows/incrementVersionAndCreatePR.cjs
@@ -33,14 +33,19 @@ module.exports = async ({ github, context, versionType }) => {
   }
 
   async function createPull(title, source, target) {
-    await github.pulls.create({
-      title,
-      body: `${title} PR.`,
-      owner: context.repo.owner,
-      repo: context.repo.repo,
-      head: source,
-      base: target,
-    });
+    try {
+      await github.pulls.create({
+        title,
+        body: `${title} PR.`,
+        owner: context.repo.owner,
+        repo: context.repo.repo,
+        head: source,
+        base: target,
+      });
+    } catch (error) {
+      console.error(error);
+      throw new Error("Error in creating pull request", error);
+    }
   }
 
   try {
